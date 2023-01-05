0cdb82b642a111782f0b938d3144940b2771eb4a
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-01-05 09:45:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -257,7 +257,7 @@ elif selected2 == 'File Search':
 
 
 elif selected2 == 'AI Assistant':
-    st.image("search1.png")
+    st.image("colab.png")
     def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)

4b458a4d5fb2afc0fe85fd34ae70c708ca2e1065
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-01-05 10:21:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,13 +106,13 @@ def torrent_download(search):
 
 
 
-# hide_menu_style = """
-#         <style>
-#         MainMenu {visibility: hidden;}
-#         footer {visibility: hidden; }
-#         </style>
-#         """
-# st.markdown(hide_menu_style, unsafe_allow_html=True)
+hide_menu_style = """
+        <style>
+        MainMenu {visibility: hidden;}
+        footer {visibility: hidden; }
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 def display(data):

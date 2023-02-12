abc104ef9f2b1f498ecf0cb772eebaf05e6ed16c
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-02-12 07:01:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def ai(prompt,n):
     model="text-davinci-003",
     prompt=prompt,
     temperature=n,
-    max_tokens=150,
+    max_tokens=2000,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,

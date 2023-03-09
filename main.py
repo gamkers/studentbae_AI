e7ed9886f67b9f95115e4e16440953a26d93c97a
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-03-09 16:13:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -28,21 +28,20 @@ openai.api_key = st.secrets["api"]
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 def sql(t,q):
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"### Postgres SQL tables, with their properties:\n#\n#{t}\n#\n###{q}\nSELECT",
-    temperature=n,
-    max_tokens=2000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
-  )
+#   response = openai.Completion.create(
+#     model="text-davinci-003",
+#     prompt=f"### Postgres SQL tables, with their properties:\n#\n#{t}\n#\n###{q}\nSELECT",
+#     temperature=n,
+#     max_tokens=2000,
+#     top_p=1,
+#     frequency_penalty=0,
+#     presence_penalty=0.6,
+#     stop=[" Human:", " AI:"]
+#   )
   
-  st.markdown(response.choices[0].text,unsafe_allow_html=True)
+#   st.markdown(response.choices[0].text,unsafe_allow_html=True)
   
 def ai(prompt,n):
-
   response = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,

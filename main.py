12ece19be3bc825d9b54bcf62b86c62215244318
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-03-09 16:36:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,26 @@ def ai(prompt,n):
   st.markdown(response.choices[0].text,unsafe_allow_html=True)
   return data
 
+openai.api_key = st.secrets["api"]
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+def sql(t,q):
+
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"###{t}\n#\n### {q}\nSELECT",
+    temperature=1,
+    max_tokens=2000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+  )
+  data = response.choices[0].text
+  st.markdown(response.choices[0].text,unsafe_allow_html=True)
+  return data
+
 def yt(vd):
     customSearch = VideosSearch(vd,limit = 20)
     for i in range(20):

46e1142517ec3e27983db50fc7c3d5b321e5394a
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-04-27 12:24:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def pearson(my_string):
   pattern = "(instagram|facebook|youtube|twitter|github|linkedin|scholar|hackerrank|tiktok|maps)+\.(com|edu|net|fandom)"
   for i in search(query, tld="co.in", num=20, stop=15, pause=2):
     if (re.search(pattern, i)):
-      title=ai(i+"what is the website",1)
+      title=ai(i+"website name in bold",1)
       st.markdown(f'<a href="{i}">view more</a>', unsafe_allow_html=True)
     else:
       print("match not found")

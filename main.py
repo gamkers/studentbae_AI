8d9519b6bf027001593caefb5e7ea97ee7bd8cf9
AKASH <87299311+gamkers@users.noreply.github.com>
2023-05-30 11:57:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -607,18 +607,17 @@ elif selected2 == 'AdvanceGPT':
         urls = pdfs(selected, 1)
         texts = pdftotxt(urls)
         chunks(texts)
-        st.write("Type your questions here")
-        selected1 = st.text_input("Widget 2", key="widget2")
-        submit1 = st.button("Button 2", key="hello")
+    st.write("Type your questions here")
+    selected1 = st.text_input("Widget 2", key="widget2")
+    submit1 = st.button("Button 2", key="hello")
+    if submit1:
+        query = selected1
+        docs = docsearch.similarity_search(query)
 
-        if submit1:
-            st.write("Type your questions here")
-            query = selected1
-            docs = docsearch.similarity_search(query)
-
-            st.write(chain.run(input_documents=docs, question="TITLE of the paper"))
-            st.write(chain.run(input_documents=docs, question=query))
-            st.write("FOR REFERENCE:", *urls)
+        st.write(chain.run(input_documents=docs, question="TITLE of the paper"))
+        st.write(chain.run(input_documents=docs, question=query))
+        st.write("FOR REFERENCE:", *urls)
+        
 
 
 

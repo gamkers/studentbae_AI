7534bf158ea754c8754f79caa4fde85d98b443d3
AKASH <87299311+gamkers@users.noreply.github.com>
2023-05-31 15:25:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -618,9 +618,8 @@ elif selected2 == 'AdvanceGPT':
         chunks(texts)
         selected1 = st.text_input("Widget 2", key="widget2")
         submit1 = st.button("Button 2", key="hello")
-        answers("what is ec2")
-        if submit1:
-            answers(selected1)
+    if submit1:
+        answers(selected1)
             
 
     

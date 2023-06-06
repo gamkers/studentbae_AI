ef6a5ba1d69ab7cc80a8cb6c8f77681f032fc873
AKASH <87299311+gamkers@users.noreply.github.com>
2023-06-06 11:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -505,7 +505,7 @@ def newsify():
             st.write("_______________________________________________________________________________")
 
 
-         def local_css(file_name):
+        def local_css(file_name):
             with open(file_name) as f:
                 st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 

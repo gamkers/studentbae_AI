98ea64766525b49b9cbbc9cbb324f25e716be1a9
AKASH <87299311+gamkers@users.noreply.github.com>
2023-06-06 12:00:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -907,7 +907,9 @@ elif selected2 == "DOCSGPT":
         docs = docsearchs.similarity_search(query)
         st.write(chain.run(input_documents=docs, question=query))
 
-elif selected2 == 'NESIFY':
+elif selected2 == 'NEWSIFY':
+    
+    st.image("colab.png")
     def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -916,12 +918,14 @@ elif selected2 == 'NESIFY':
     def remote_css(url):
         st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
 
+
     def icon(icon_name):
         st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
 
     local_css("style.css")
     remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+  
     st.header("Explore The World")
     form = st.form(key='my-form')
     selected = form.text_input("", "")
@@ -932,7 +936,7 @@ elif selected2 == 'NESIFY':
             )
 
     n = st.slider('News Count', 0, 130, 25)
-    newsify()
+ 
     
 
 

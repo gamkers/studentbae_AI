3998f61c223074b541ca5b85af36c979e005ce45
AKASH <87299311+gamkers@users.noreply.github.com>
2023-06-10 12:05:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import openai
 from youtubesearchpython import *
 import io
 import requests
-import webview
 import PyPDF2
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings

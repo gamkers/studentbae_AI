532897375409323f731e62c807fc1fedf3c4cbd4
AKASH <87299311+gamkers@users.noreply.github.com>
2023-07-04 12:57:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -460,8 +460,103 @@ def displays(data):
 
         st.write("_______________________________________________________________________________")
 
+
+
+def register():
+    st.title("User Registration")
+    
+    with st.form("registration_form"):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        confirm_password = st.text_input("Confirm Password", type="password")
+        
+        # Validate password
+        if st.form_submit_button("Validate Password"):
+            if is_strong_password(password):
+                st.success("Password meets the strength criteria.")
+            else:
+                st.error("Password must contain at least 8 characters, including uppercase, lowercase, and special characters.")
+    
+    st.subheader("API Key")
+    st.markdown("To complete the registration, please provide your API key.")
+    st.markdown("You can obtain an API key from OpenAI by following these steps:")
+    st.markdown("1. Go to the OpenAI website at [https://openai.com](https://openai.com).")
+    st.markdown("2. Sign in to your OpenAI account or create a new account if you don't have one.")
+    st.markdown("3. Once signed in, navigate to your account settings or dashboard.")
+    st.markdown("4. Look for the API Key section or API Key management.")
+    st.markdown("5. Generate a new API key for your application.")
+    st.markdown("6. Copy the generated API key.")
+    st.markdown("7. Return to this registration page.")
+    
+    with st.form("api_key_form"):
+        api_key = st.text_input("API Key")
+        
+        if st.form_submit_button("Complete Registration"):
+            if api_key:
+                if is_username_available(username):
+                    deta = Deta(st.secrets["data_key"])
+                    db = deta.Base("USERS")
+                    db.put({"username": username, "password": password, "api_key": api_key})
+                    st.success("Registration Successful. Please log in.")
+                else:
+                    st.error("Username already exists. Please choose a different username.")
+            else:
+                st.warning("Please provide the API key to complete the registration.")
+
+def is_username_available(username):
+    deta = Deta(st.secrets["data_key"])
+    db = deta.Base("USERS")
+    db_content = db.fetch().items
+
+    for item in db_content:
+        if item["username"] == username:
+            return False
+    
+    return True
+
+def is_strong_password(password):
+    # Password must contain at least 8 characters, including uppercase, lowercase, and special characters
+    if len(password) < 8:
+        return False
+    if not re.search(r"[A-Z]", password):
+        return False
+    if not re.search(r"[a-z]", password):
+        return False
+    if not re.search(r"\W", password):
+        return False
+    
+    return True
+
+
+def login():
+    # global log
+    st.title("User Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if valid_credentials(username, password):
+            st.success("Login Successful")
+            log=1
+            return True
+        else:
+            st.error("Incorrect username or password. Please try again.")
+
+
+
+def valid_credentials(username, password):
+    deta = Deta(st.secrets["data_key"])
+    db = deta.Base("USERS")
+    db_content = db.fetch().items
+
+    for item in db_content:
+        if item["username"] == username and item["password"] == password:
+            return True
+    
+    return False
+
+
 with st.sidebar:
-  selected2 = option_menu(None, ["Home","Assistant",'Search','AdvanceGPT','PDF', 'PPT', 'Courses', 'Research papers','Question Papers', 'E-BOOKS',"SQL",'OSINT',"DOCSGPT",'NEWSIFY'],
+  selected2 = option_menu(None, ["Login","Register","Home","Assistant",'Search','AdvanceGPT','PDF', 'PPT', 'Courses', 'Research papers','Question Papers', 'E-BOOKS',"SQL",'OSINT',"DOCSGPT",'NEWSIFY'],
                           icons=['house','robot','files'],
                           menu_icon="cast", default_index=2, orientation="vertical")
 
@@ -476,7 +571,11 @@ lottie_coding = lottieurl("https://assets10.lottiefiles.com/packages/lf20_i9mtrv
 lottie_coding2 = lottieurl("https://assets8.lottiefiles.com/packages/lf20_2LdLki.json")
 lottie_coding3 = lottieurl("https://assets8.lottiefiles.com/packages/lf20_oyi9a28g.json")
 
-if selected2 == 'Home':
+if selected2 == 'Login':
+    login()
+elif selected2 == 'Register':
+    register()
+elif selected2 == 'Home':
     st.image("logofinal.png")
     with st.container():
         st.write("---")

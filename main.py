2b0864f4b3663b895056315b29fc8a0821dae7aa
AKASH <87299311+gamkers@users.noreply.github.com>
2023-07-25 11:18:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ opacity: 0.8;
 """
 
 
-#st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 openai.api_key = st.secrets["api"]
 start_sequence = "\nAI:"

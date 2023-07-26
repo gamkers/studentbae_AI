426cf61488caad85427273429bb0b095c0476fba
AKASH <87299311+gamkers@users.noreply.github.com>
2023-07-26 17:04:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -116,11 +116,8 @@ def pdftotxt(urls):
     
 def chunks(texts,q):
 
-    progress_text = "DATA TRANFORMATION STARTED"
-    my_bar = st.progress(0, text=progress_text)
-    for percent_complete in range(100):
-        time.sleep(0.1)
-        my_bar.progress(percent_complete + 1, text=progress_text)
+    st.info("DATA TRANFORMATION STARTED")
+
     st.info("TRANFORMING DATA INTO CHUNKS")
     text_splitter = CharacterTextSplitter(separator = "\n",chunk_size = 1000,chunk_overlap  = 200,
     length_function = len,)

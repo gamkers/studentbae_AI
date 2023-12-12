bac2bbe292bc814363037c740f329f45b45e17d6
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-12 12:43:51 +0530

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -47,10 +47,11 @@ def palm_pdf(txt):
 
     return(completion.result)
 
-def palm(txt):
+def ai_palm(txt):
+    import google.generativeai as palm
+    palm.configure(api_key=api_key=st.secrets["palm_api"])
     models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
     model = models[0].name
-    print(model)
     prompt = f"""
     You are an expert at Teaching.
 

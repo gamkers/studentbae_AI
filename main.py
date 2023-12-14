a9de9c07449ee7d6ca985f722abff05a6c4d0e78
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-14 18:14:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def lottieurl(url):
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: linear-gradient(to right, #008080, #008080);
+background-image: linear-gradient(to right, #000000, #000000);
 opacity: 0.8;
 
 }}

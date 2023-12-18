6b8507da7f3885e0773043d1fc0b26c53d76a2eb
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-18 17:45:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ try:
 
         form = st.form(key='my-form')
 
-        selected = form.text_input("What are you Looking for?", "")
+        selected = st.chat_input("What are you Looking for?", "")
         submit = form.form_submit_button("SEARCH")
 
         options = st.multiselect(

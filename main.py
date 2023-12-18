3fb5bc5b52f068a9693e549449f70d8f33f3ff62
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-18 15:48:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -246,22 +246,24 @@ try:
         if submit:
             ai_HR(t)
     elif selected2 == 'OSINT':
-        st.image("images/search1.png")
-        def local_css(file_name):
-                with open(file_name) as f:
-                    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-        def remote_css(url):
-                st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
-        def icon(icon_name):
-                st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+        # Run the conversation function
+        palm_conversation()
+        # st.image("images/search1.png")
+        # def local_css(file_name):
+        #         with open(file_name) as f:
+        #             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        # def remote_css(url):
+        #         st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+        # def icon(icon_name):
+        #         st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
             
-        local_css("style.css")
-        remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
-        #   form = st.form(key='my-form')
-        t = st.text_input("USERNAME", "")
-        submit = st.button("SEARCH")
-        if submit:
-            pearson(t)
+        # local_css("style.css")
+        # remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+        # #   form = st.form(key='my-form')
+        # t = st.text_input("USERNAME", "")
+        # submit = st.button("SEARCH")
+        # if submit:
+        #     pearson(t)
         
     
     elif selected2 == 'Assistant':

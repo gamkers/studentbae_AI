3f8b98e342abc8ea0bb7b92c5d48ab07c9806f9a
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-18 13:13:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from scripts.login import *
 st.set_page_config(page_title="STUDENTBAE", page_icon=":tada:", layout='wide')
 from scripts.functions  import *
 
-
+log = False
 def displays(data):
     voice = []
     for i in range(5):
@@ -325,6 +325,10 @@ try:
             
     elif selected2 == 'AdvanceGPT':
         st.image("images/colab.png")
+        if log == True:
+            st.write("Login Successful")
+        else:
+            st.write("Login Failes")
         def local_css(file_name):
             with open(file_name) as f:
                 st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)

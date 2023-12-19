1c6d32d78f051136e09cd1cf3ac3465e9f207b72
AKASH <87299311+gamkers@users.noreply.github.com>
2023-12-19 17:04:57 +0530

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -101,9 +101,11 @@ def palm_conversation(context=""):
     st.markdown("##Your AI Teacher - You can clear your doubts here")
     st.markdown("")
     image = st.file_uploader(label="Upload your image here", type=['png', 'jpg', 'jpeg'])
-    submit = st.button("Clear Chat")
+    submit = st.button("Answer")
     if submit:
         st.session_state["data"] = " "
+        st.session_state["pal_context"] = ""
+        st.session_state.messages = []
     if image is not None and st.session_state["data"] == " ":
         input_image = Image.open(image)
         st.image(input_image)

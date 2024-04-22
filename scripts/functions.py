38d74f7ab48c4c0a333ad4ba65c111ca0e75e929
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-22 10:31:56 +0530

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -639,15 +639,14 @@ def get_conversation_chain():
 
     # define the prompt
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model = "gemini-pro", temperatue = 0.3,google_api_key=st.secrets["gemini_api"]) # create object of gemini-pro
+    model = ChatGoogleGenerativeAI(model = "gemini-pro", temperatue = 0.6,google_api_key=st.secrets["gemini_api"]) # create object of gemini-pro
 
     prompt = PromptTemplate(template = prompt_template, input_variables= ["context","question"])
 

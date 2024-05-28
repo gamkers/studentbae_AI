9583006d350f3322786700ca87ef1095ce0d4051
AKASH <87299311+gamkers@users.noreply.github.com>
2024-05-28 12:01:01 +0530

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -623,7 +623,7 @@ def get_pdf_text(pdf_docs):
     return text
 
 def get_text_chunks(text):
-    create an object of RecursiveCharacterTextSplitter with specific chunk size and overlap size
+    #create an object of RecursiveCharacterTextSplitter with specific chunk size and overlap size
     # text_splitter = CharacterTextSplitter(separator = "\n",chunk_size = 1000,chunk_overlap  = 200,
     #   length_function = len,)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
